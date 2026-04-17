3e906f067664a9c8d1e700b5e51b2a19be869d9d
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-04-17 15:55:25 +0300

    Update AntiDropshotPlugin.cs

--- AntiDropshotPlugin.cs
+++ AntiDropshotPlugin.cs
@@ -3,6 +3,7 @@ using Rocket.Core.Plugins;
 using Rocket.Unturned;
 using Rocket.Unturned.Player;
 using SDG.Unturned;
+using HarmonyLib;
 using Logger = Rocket.Core.Logging.Logger;
 
 namespace AntiDropshot
@@ -10,19 +11,30 @@ namespace AntiDropshot
     public class AntiDropshotPlugin : RocketPlugin<AntiDropshotConfig>
     {
         public static AntiDropshotPlugin Instance { get; private set; }
+        private Harmony _harmony;
 
         protected override void Load()
         {
             Instance = this;
+
+            // Инициализация Harmony для работы патчей
+            _harmony = new Harmony("com.project.antidropshot");
+            _harmony.PatchAll();
+
             U.Events.OnPlayerConnected += OnPlayerConnected;
             
             Logger.Log("--- ANTIDROPSHOT v1.0 Loaded ---");
+            Logger.Log("Harmony patches applied successfully.");
             Logger.Log($"Crouch Cost: {Configuration.Instance.StaminaCostCrouch} | Prone Cost: {Configuration.Instance.StaminaCostProne}");
         }
 
         protected override void Unload()
         {
             U.Events.OnPlayerConnected -= OnPlayerConnected;
+            
+            // Снимаем патчи при выгрузке
+            _harmony.UnpatchAll("com.project.antidropshot");
+            
             Instance = null;
             Logger.Log("--- ANTIDROPSHOT Unloaded ---");
         }
