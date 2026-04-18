8cf971b1b01c63ee15dba5f1f528986649f22c01
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-04-18 14:20:29 +0300

    Update StancePatch.cs

--- StancePatch.cs
+++ StancePatch.cs
@@ -1,22 +1,27 @@
 using HarmonyLib;
 using SDG.Unturned;
+using System;
 using System.Diagnostics;
 
 namespace AntiDropshot
 {
-    [HarmonyPatch(typeof(PlayerStance), nameof(PlayerStance.checkStance))]
+    // Явно указываем типы аргументов: (EPlayerStance, bool), чтобы избежать AmbiguousMatchException
+    [HarmonyPatch(typeof(PlayerStance), nameof(PlayerStance.checkStance), new Type[] { typeof(EPlayerStance), typeof(bool) })]
     public static class PlayerStance_CheckStance_Patch
     {
         [HarmonyPrefix]
-        public static void Prefix(PlayerStance __instance, EPlayerStance newStance)
+        public static void Prefix(PlayerStance __instance, EPlayerStance newStance, bool force)
         {
+            // Если поза фактически не меняется, игнорируем
             if (__instance.stance == newStance) return;
 
+            // Захватываем стек вызовов
             StackTrace stackTrace = new StackTrace();
             StackFrame[] frames = stackTrace.GetFrames();
 
             bool isExternalPlugin = false;
 
+            // Проверяем, кто вызвал метод
             for (int i = 1; i < frames.Length; i++)
             {
                 var method = frames[i].GetMethod();
@@ -24,6 +29,7 @@ namespace AntiDropshot
 
                 string assemblyName = method.DeclaringType.Assembly.GetName().Name;
 
+                // Если вызов НЕ от игры, НЕ от движка и НЕ от нашего плагина
                 if (assemblyName != "Assembly-CSharp" && 
                     assemblyName != "UnityEngine.CoreModule" &&
                     assemblyName != "Rocket.Unturned" &&
@@ -39,6 +45,7 @@ namespace AntiDropshot
                 var comp = __instance.player.GetComponent<AntiDropshotComponent>();
                 if (comp != null)
                 {
+                    // Сообщаем компоненту принять позу "молча" (без штрафов и задержек)
                     comp.SilentlyAcceptStance(newStance);
                 }
             }
